0ddd59b29a027dd553f841c699cf84b02425f519
tarunmandhan <mandhantarun@gmail.com>
2022-12-21 03:07:17 +0530

    Q & A with id

---
+++ src/Api.js
@@ -0,0 +1,39 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Ques:- What is the virtual DOM?",
+    answer:
+      " Ans:- DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree structure. ",
+  },
+  {
+    id: 2,
+    question: "How do you create a React app?",
+    answer:
+      " Ans:- Install the create-react-app package using the command prompt or terminal. ",
+  },
+  {
+    id: 3,
+    question: "Ques:- What is an event in React?",
+    answer:
+      " Ans:- An event is an action that a user or system may trigger, such as pressing a key , a mouse click. etc ",
+  },
+
+  {
+    id: 4,
+    question: "Ques:- What are synthetic events in React?",
+    answer:
+      " Ans:- Synthetic events combine the response of different browser's native events into API,esuring that the",
+  },
+  {
+    id: 5,
+    question: "Ques:- What are the components in React?",
+    answer:
+      " Ans:- Components are the building blocks of any React application, and a single app usually consists of multiple",
+  },
+  {
+    id: 6,
+    question: "Ques:- What are the components in tarun ?",
+    answer:
+      " Ans:- Components are the building blocks of any React application, and a single app usually consists of multiple",
+  },
+];
